b0acefc6abf3e26668f7ede60e15781d3c8ddf08
AnamarieHa <anamarie.ha@gmail.com>
2018-01-09 23:41:10 -0800

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,27 +1,30 @@
-
+/* class creates a new account for each player
+* holds information about account number and balance
+* allows the program to withdraw, deposit, and get the balance
+*/
 public class Account 
 {
-	private int accNum;
+	private int accNum; 
 	private double balance;
-	public Account(int num)
+	public Account(int num) // creates a new account without money
 	{
 		this.accNum = num;
 		this.balance = 0;
 	}
-	public Account(int num, double balance)
+	public Account(int num, double balance) // creates a new account with the given balance
 	{
 		this.accNum = num;
 		this.balance = balance;
 	}
-	public void withdraw(double amount)
+	public void withdraw(double amount) // method subtracts the given amount from an account
 	{
 		this.balance -= amount;
 	}
-	public void deposit(double amount)
+	public void deposit(double amount) // method adds the given amoutn to an account
 	{
 		this.balance += amount;
 	}
-	public double getBalance()
+	public double getBalance()  // method returns the balance of an account
 	{
 		return this.balance;
 	}
